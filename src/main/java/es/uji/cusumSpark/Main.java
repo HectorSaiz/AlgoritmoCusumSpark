e9e5cc81a8df2334f06ca4585b906b21510cb8ed
HectorSaiz <al187091@uji.es>
2015-07-01 17:41:47 +0200

    Incluidas marcas de tiempo

--- src/main/java/es/uji/cusumSpark/Main.java
+++ src/main/java/es/uji/cusumSpark/Main.java
@@ -4,7 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        CusumSpark.realizaExperimentos();
+        long time_start, time_end;
+
+        System.out.println("Inicia tarea principal");
+
+        time_start = System.currentTimeMillis();
+        System.out.println("Arrancan los experimentos");
+        CusumSpark.realizaExperimentos(); // llamamos a la tarea
+        time_end = System.currentTimeMillis();
+
+        System.out.println("Tiempo empleado " + (time_end - time_start) / 1000 + " segundos || " + (time_end - time_start) / 1000 / 60 + " minutos" );
+
+
 
     }
 }
